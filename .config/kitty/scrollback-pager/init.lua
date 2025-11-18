1a89c5865d43772fa07c73fe7052962fe90ed983
Pierluigi Di Cerbo <pierluigi.dicerbo@gmail.com>
2025-11-18 12:59:39 +0100

    conflict keybind removed from hyprland config; kitty config improved by adding neovim as scrollback_pager tool

---
+++ .config/kitty/scrollback-pager/init.lua
@@ -0,0 +1,42 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+-- vim.cmd("set guicursor=n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.background = "dark"
+vim.opt.cursorline = true -- Enable highlighting of the current line
+vim.opt.clipboard:append ("unnamedplus") -- Copy paste between vim and everything else
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.hlsearch = true -- Highlight all matches on previous search pattern
+vim.opt.list = true -- Show some invisible characters (tabs...
+vim.opt.number = true -- Print line number
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.showmode = false -- Dont show mode since we have a statusline
+vim.opt.smartindent = true -- Insert indents automatically
+vim.opt.softtabstop = 4
+vim.opt.spelllang = { "en" }
+vim.opt.splitbelow = true -- Split horizontally to the bottom
+vim.opt.splitright = true -- Split vertically to the right
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+vim.opt.winborder = 'rounded'
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.readonly = true
+vim.opt.modifiable = false
+
+-- Keep cursor in the middle of the screen when jumping up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move down in buffer with cursor centered" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up in buffer with cursor centered" })
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "q", ":q<CR>")
+
+-- toggle line wrap
+vim.keymap.set("n", "<leader>wl", "<cmd>set wrap!<CR>", { desc = "Toggle line wrap" })
+
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
