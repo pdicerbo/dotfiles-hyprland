3ded2f4b380263c21d7b06693bd406182ad2f34a
Pierluigi Di Cerbo <pierluigi.dicerbo@gmail.com>
2026-04-01 12:42:30 +0200

    termite plugin added, yanky keybinds fixed to avoid overlap with split motions ones (repo aligned to commit 241c5357 of the old one)

---
+++ .config/nvim/lua/plugins/termite.lua
@@ -0,0 +1,16 @@
+return {
+    -- Stacking float terminal manager for Neovim.
+    "ruicsh/termite.nvim",
+    opts = {
+        height = 0.35,
+        position = "bottom",
+        winbar = false,
+        keymaps = {
+            prev = "<C-j>",
+            next = "<C-k>",
+        },
+        highlights = {
+            border_active   = { fg = "#ff9e64", bg = "NONE" },
+        }
+    }
+}
